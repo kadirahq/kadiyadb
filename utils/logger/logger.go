f0e52e9e2314a4f685b6a94ee6025859f3a6bcfe
Muhammed Thanish <mnmtanish@gmail.com>
2015-07-25 06:55:19 +0530

    Log on by default

--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -6,14 +6,26 @@ import (
 )
 
 var (
-	enabled = os.Getenv("debug") != ""
-	logger  = log.New(os.Stdout, "", log.LstdFlags)
+	enableLog   = true
+	enabldDebug = os.Getenv("DEBUG") != ""
+
+	logger = log.New(os.Stdout, "", log.LstdFlags)
 )
 
 // Log prints the error message with a prefix if the `debug` env variable
 // is set (to something other than an empty string of course)
-func Log(prefix string, err error) {
-	if enabled {
-		logger.Printf("%s: %s\n", prefix, err.Error())
+func Log(prefix string, logs ...interface{}) {
+	if enableLog {
+		logger.Printf("%s: ", prefix)
+		logger.Println(logs...)
+	}
+}
+
+// Debug prints the error message with a prefix if the `debug` env variable
+// is set (to something other than an empty string of course)
+func Debug(prefix string, logs ...interface{}) {
+	if enabldDebug {
+		logger.Printf("%s: ", prefix)
+		logger.Println(logs...)
 	}
 }
