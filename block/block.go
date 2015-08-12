bd364195057d8d1d7fda51ad7cbb45aeb93389e3
Muhammed Thanish <mnmtanish@gmail.com>
2015-08-12 13:04:31 +0530

    Rewrite block package

--- block/block.go
+++ block/block.go
@@ -3,37 +3,29 @@ package block
 import (
 	"errors"
 	"os"
-	"path"
-	"sync/atomic"
+	"sync"
 
-	"github.com/kadirahq/kadiyadb/utils/logger"
-	"github.com/kadirahq/kadiyadb/utils/mdata"
+	"github.com/kadirahq/go-tools/logger"
+	"github.com/kadirahq/go-tools/segfile"
 )
 
 const (
-	// SegPrefix is the name prefixed to each segment file.
-	// Segment files are numbered from 0 e.g. "seg_0, seg_1, seg_2, ..."
-	SegPrefix = "seg_"
-
-	// MDFileName is the metadata file which has segment information.
-	// This file is stored in same directory with all segment files.
-	MDFileName = "metadata"
-
-	// MDHeaderSz is the size of the header info stored with metadata
-	// Currently the header only contains a single `uint32` to store the size.
-	MDHeaderSz = 4
-
-	// PreallocThresh is the minimum number record space to keep available
-	// to have ready. Additional segments will be created automatically.
-	PreallocThresh = 1000
+	// FilePrefix will be prefixed to segment files
+	FilePrefix = "block_"
 )
 
 var (
-	// ErrWrite is returned when number of bytes doesn't match data size
-	ErrWrite = errors.New("number of bytes written doesn't match data size")
+	// ErrClose is returned when close is closed multiple times
+	ErrClose = errors.New("close called multiple times")
 
-	// ErrRead is returned when number of bytes doesn't match data size
-	ErrRead = errors.New("number of bytes read doesn't match data size")
+	// ErrWrite is returned when bytes written is not equal to data size
+	ErrWrite = errors.New("bytes written != data size")
+
+	// ErrRead is returned when bytes read is not equal to data size
+	ErrRead = errors.New("bytes read != data size")
+
+	// ErrOptions is returned when options have missing or invalid fields.
+	ErrOptions = errors.New("invalid or missing options")
 
 	// ErrBound is returned when requested start,end times are invalid
 	ErrBound = errors.New("requested start/end times are invalid")
@@ -44,20 +36,35 @@ var (
 	// ErrNoBlock is returned when the block is not found on disk (read-only).
 	ErrNoBlock = errors.New("requested block is not available on disk")
 
+	// ErrNoRec is returned when the record is not found on block
+	ErrNoRec = errors.New("record is not found on block")
+
 	// ErrPSize is returned when payload size doesn't match block options
 	ErrPSize = errors.New("payload size is not compatible with block")
+
+	// ErrCorrupt is returned when the segfile is corrupt
+	ErrCorrupt = errors.New("segfile is corrupt")
 
 	// Logger logs stuff
 	Logger = logger.New("BLOCK")
 )
 
-// Options has parameters required for creating a block
+// Options for new block
 type Options struct {
-	Path  string // files stored under this directory
-	PSize uint32 // number of bye
-	RSize uint32 // number of payloads in a record
-	SSize uint32 // number of records in a segment
-	ROnly bool   // read only or read/write block
+	// directory to store block files
+	Path string
+
+	// number of bytes in a payload
+	PSize uint32
+
+	// number of payloads in a record
+	RSize uint32
+
+	// number of records in a segment
+	SSize uint32
+
+	// no writes allowed
+	ROnly bool
 }
 
 // Block is a collection of records which contains a series of fixed sized
@@ -83,18 +90,39 @@ type Block interface {
 }
 
 type block struct {
-	options  *Options   // options
-	metadata *Metadata  // metadata contains segment details
-	mdstore  mdata.Data // persistence helper for metadata
-	metrics  *Metrics   // performance metrics
+	// segmented file with block data
+	data segfile.File
+
+	// add mutex to control adds
+	mutex *sync.Mutex
+
+	// set to true when the file is closed
+	closed bool
+
+	// set to true when the file is read only
+	ronly bool
+
+	// payload size
+	psize uint32
+
+	// record size
+	rsize uint32
 }
 
 // New creates a `Block` to store or get (time) series data.
-// The `ROnly` option determines whether it'll be a read-only (roblock)
-// or a writable (rwblock). It also loads metadata from disk if available.
-// For read-only blocks, it'll test whether the block exists by checking
-// whether the directory of the block exists and program can access it.
+// It'll test whether the block exists by checking whether the directory
+// of the block exists and whether the program has permissions to access it.
 func New(options *Options) (b Block, err error) {
+	// validate options
+	if options == nil ||
+		options.Path == "" ||
+		options.PSize <= 0 ||
+		options.RSize <= 0 ||
+		options.SSize <= 0 {
+		Logger.Trace(ErrOptions)
+		return nil, ErrOptions
+	}
+
 	if options.ROnly {
 		err = os.Chdir(options.Path)
 
@@ -108,56 +136,158 @@ func New(options *Options) (b Block, err error) {
 		}
 	}
 
-	metadata := &Metadata{
-		Capacity: options.SSize,
-		Segments: 0,
-		Records:  0,
+	ssize := options.SSize * options.RSize * options.PSize
+	so := &segfile.Options{
+		Path:      options.Path,
+		Prefix:    FilePrefix,
+		FileSize:  int64(ssize),
+		ReadOnly:  options.ROnly,
+		MemoryMap: !options.ROnly,
 	}
 
-	mdpath := path.Join(options.Path, MDFileName)
-	mdstore, err := mdata.New(mdpath, metadata, options.ROnly)
+	sf, err := segfile.New(so)
 	if err != nil {
 		Logger.Trace(err)
 		return nil, err
 	}
 
-	cb := &block{
-		options:  options,
-		metadata: metadata,
-		metrics:  &Metrics{},
+	meta := sf.Info()
+	if meta.DataSize%int64(options.RSize*options.PSize) != 0 {
+		Logger.Trace(ErrCorrupt)
+		return nil, ErrCorrupt
 	}
 
-	if options.ROnly {
-		err = mdstore.Close()
-		if err != nil {
-			Logger.Error(err)
-		}
-
-		return NewRO(cb, options)
+	b = &block{
+		data:  sf,
+		mutex: &sync.Mutex{},
+		ronly: options.ROnly,
+		psize: options.PSize,
+		rsize: options.RSize,
 	}
 
-	cb.mdstore = mdstore
-	return NewRW(cb, options)
+	return b, nil
+}
+
+func (b *block) Add() (id uint32, err error) {
+	if b.ronly {
+		Logger.Trace(ErrROnly)
+		return 0, ErrROnly
+	}
+
+	rsize := int64(b.rsize * b.psize)
+
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	meta := b.data.Info()
+
+	if meta.DataSize%rsize != 0 {
+		Logger.Trace(ErrCorrupt)
+		return 0, ErrCorrupt
+	}
+
+	id = uint32(meta.DataSize / rsize)
+
+	err = b.data.Grow(rsize)
+	if err != nil {
+		Logger.Trace(err)
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (b *block) Put(id, pos uint32, pld []byte) (err error) {
+	if b.ronly {
+		Logger.Trace(ErrROnly)
+		return ErrROnly
+	}
+
+	if pos >= b.rsize || pos < 0 {
+		Logger.Trace(ErrBound)
+		return ErrBound
+	}
+
+	if len(pld) != int(b.psize) {
+		Logger.Trace(ErrPSize)
+		return ErrPSize
+	}
+
+	meta := b.data.Info()
+	rsize := int64(b.rsize * b.psize)
+
+	if meta.DataSize%rsize != 0 {
+		Logger.Trace(ErrCorrupt)
+		return ErrCorrupt
+	}
+
+	nextID := uint32(meta.DataSize / rsize)
+	if id >= nextID {
+		Logger.Trace(ErrNoRec)
+		return ErrNoRec
+	}
+
+	roff := id * b.rsize * b.psize
+	offset := int64(roff + pos*b.psize)
+
+	n, err := b.data.WriteAt(pld, offset)
+	if err != nil {
+		Logger.Trace(err)
+		return err
+	} else if n != int(b.psize) {
+		Logger.Trace(ErrWrite)
+		return ErrWrite
+	}
+
+	return nil
+}
+
+func (b *block) Get(id, start, end uint32) (res [][]byte, err error) {
+	if end > b.rsize || start < 0 || end < start {
+		Logger.Trace(ErrBound)
+		return nil, ErrBound
+	}
+
+	count := end - start
+	size := count * b.psize
+	data := make([]byte, size)
+	roff := id * b.rsize * b.psize
+	offset := int64(roff + start*b.psize)
+
+	n, err := b.data.ReadAt(data, offset)
+	if err != nil {
+		Logger.Trace(err)
+		return nil, err
+	} else if uint32(n) != size {
+		Logger.Trace(ErrRead)
+		return nil, ErrRead
+	}
+
+	res = make([][]byte, count)
+
+	var i uint32
+	for i = 0; i < count; i++ {
+		res[i] = data[i*b.psize : (i+1)*b.psize]
+	}
+
+	return res, nil
 }
 
 func (b *block) Metrics() (m *Metrics) {
-	metrics := *b.metrics
-	metrics.Capacity = int64(b.metadata.Capacity)
-	metrics.Segments = int64(b.metadata.Segments)
-	metrics.Records = int64(b.metadata.Records)
-	atomic.StoreInt64(&b.metrics.AddOps, 0)
-	atomic.StoreInt64(&b.metrics.PutOps, 0)
-	atomic.StoreInt64(&b.metrics.GetOps, 0)
-	return &metrics
+	// TODO code!
+	return nil
 }
 
 func (b *block) Close() (err error) {
-	if b.mdstore != nil {
-		err = b.mdstore.Close()
-		if err != nil {
-			Logger.Trace(err)
-			return err
-		}
+	if b.closed {
+		Logger.Error(ErrClose)
+		return nil
+	}
+
+	err = b.data.Close()
+	if err != nil {
+		Logger.Trace(err)
+		return err
 	}
 
 	return nil
