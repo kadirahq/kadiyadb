802e7957b63ae371234e7cf1f6e8c52057730682
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-08 14:00:56 +0530

    Improve comments for epoch package

--- epoch/cache.go
+++ epoch/cache.go
@@ -10,16 +10,22 @@ import (
 )
 
 const (
-	// AllItems removes all
-	AllItems = math.MaxInt64
+	// ExpireAll has the maximum possible value for the int64 type.
+	// Passing this for the expire function will expire all epochs.
+	ExpireAll = math.MaxInt64
 )
 
+// item structs are used as items in caches to store epochs and their weights.
+// Items with a higher number for weights are considered important so they less
+// likely to be removed when the cache runs out of space.
 type item struct {
-	value int64
-	epoch *Epoch
+	weight int64
+	epoch  *Epoch
 }
 
-// Cache is an LRU cache
+// Cache is an LRU cache for epochs. The cache contains both read-only epochs
+// and read-write epochs. An epoch can only be in one of these categories.
+// The cache has separate limits for the number of read-only/read-write epochs.
 type Cache struct {
 	rosize int64
 	rodata map[int64]*item
@@ -31,7 +37,7 @@ type Cache struct {
 	rsize  int64
 }
 
-// NewCache crates an LRU cache with given max size
+// NewCache crates an LRU cache with given RO/RW size limits
 func NewCache(rwsz, rosz int64, dir string, rsz int64) (c *Cache) {
 	return &Cache{
 		rosize: rosz,
@@ -52,13 +58,13 @@ func (c *Cache) LoadRO(key int64) (epoch *Epoch, err error) {
 
 	if item, ok := c.rwdata[key]; ok {
 		nextID := atomic.AddInt64(&c.nextID, 1)
-		atomic.StoreInt64(&item.value, nextID)
+		atomic.StoreInt64(&item.weight, nextID)
 		return item.epoch, nil
 	}
 
 	if item, ok := c.rodata[key]; ok {
 		nextID := atomic.AddInt64(&c.nextID, 1)
-		atomic.StoreInt64(&item.value, nextID)
+		atomic.StoreInt64(&item.weight, nextID)
 		return item.epoch, nil
 	}
 
@@ -73,11 +79,11 @@ func (c *Cache) LoadRO(key int64) (epoch *Epoch, err error) {
 	// add new item to the collection
 	nextID := atomic.AddInt64(&c.nextID, 1)
 	c.rodata[key] = &item{
-		value: nextID,
-		epoch: epoch,
+		weight: nextID,
+		epoch:  epoch,
 	}
 
-	c.enforceSize(true)
+	c.enforceSizeRO()
 
 	return epoch, nil
 }
@@ -95,7 +101,7 @@ func (c *Cache) LoadRW(key int64) (epoch *Epoch, err error) {
 
 	if item, ok := c.rwdata[key]; ok {
 		nextID := atomic.AddInt64(&c.nextID, 1)
-		atomic.StoreInt64(&item.value, nextID)
+		atomic.StoreInt64(&item.weight, nextID)
 		return item.epoch, nil
 	}
 
@@ -114,42 +120,17 @@ func (c *Cache) LoadRW(key int64) (epoch *Epoch, err error) {
 	// add new item to the collection
 	nextID := atomic.AddInt64(&c.nextID, 1)
 	c.rwdata[key] = &item{
-		value: nextID,
-		epoch: epoch,
+		weight: nextID,
+		epoch:  epoch,
 	}
 
-	c.enforceSize(false)
+	c.enforceSizeRW()
 
 	return epoch, nil
 }
 
-func (c *Cache) enforceSize(ronly bool) {
-	var data map[int64]*item
-	var size int64
-
-	if ronly {
-		data, size = c.rodata, c.rosize
-	} else {
-		data, size = c.rwdata, c.rwsize
-	}
-
-	for len(data) > int(size) {
-		var minKey int64
-		var minEl *item
-
-		for k, el := range data {
-			if minEl == nil || minEl.value > el.value {
-				minEl = el
-				minKey = k
-			}
-		}
-
-		delete(data, minKey)
-		minEl.epoch.Close()
-	}
-}
-
-// Expire removes all epochs from cache
+// Expire removes all epochs from cache which are older than given timestamp
+// To remove all epochs, use ExpireAll (maximum int64 value) as the timestamp.
 func (c *Cache) Expire(ts int64) {
 	todo := make(map[int64]*item, c.rosize)
 
@@ -181,7 +162,7 @@ func (c *Cache) Sync() (err error) {
 	return nil
 }
 
-// Close closes the cache
+// Close releases resources
 func (c *Cache) Close() (err error) {
 	c.mapmtx.Lock()
 	defer c.mapmtx.Unlock()
@@ -199,4 +180,32 @@ func (c *Cache) Close() (err error) {
 	}
 
 	return nil
+}
+
+// enforceSizeRO checks size limits for read-only epochs
+func (c *Cache) enforceSizeRO() {
+	c.enforceSize(c.rodata, c.rosize)
+}
+
+// enforceSizeRW checks size limits for read-write epochs
+func (c *Cache) enforceSizeRW() {
+	c.enforceSize(c.rwdata, c.rwsize)
+}
+
+// enforceSize checks size limits for given data map and size
+func (c *Cache) enforceSize(data map[int64]*item, size int64) {
+	for len(data) > int(size) {
+		var minKey int64
+		var minEl *item
+
+		for k, el := range data {
+			if minEl == nil || minEl.weight > el.weight {
+				minEl = el
+				minKey = k
+			}
+		}
+
+		delete(data, minKey)
+		minEl.epoch.Close()
+	}
 }
