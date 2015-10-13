3cdbf43f41e755464b206d49f6df3579aa9636ed
Aruna Herath <aruna@kadira.io>
2015-10-13 14:39:54 +0530

    Add test tracker and fetcher to be used on bench mark tests etc.

---
+++ test/tracker/tracker.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/kadirahq/kadiyadb/client"
+	"github.com/kadirahq/kadiyadb/server"
+)
+
+func main() {
+	var addr string
+	flag.StringVar(&addr, "addr", "localhost:8000", "Host and port of the server <host>:<port>")
+
+	flag.Parse()
+
+	c := client.New()
+	err := c.Connect(addr)
+
+	if err != nil {
+		fmt.Println("client gives error", err)
+		return
+	}
+
+	testReqs := []*server.ReqTrack{}
+
+	for i := 0; i < 100; i++ {
+		testReqs = append(testReqs, &server.ReqTrack{
+			Database: "kadiyadb",
+			Time:     uint64(i * 60000000000),
+			Fields:   []string{"foo", "bar"},
+			Total:    3.14,
+			Count:    1,
+		})
+	}
+
+	start := time.Now()
+	for i := 0; ; i++ {
+		c.Track(testReqs)
+		if i%1000 == 0 {
+			elapsed := time.Since(start)
+			log.Printf("rps %2f: ", 100*float64(1000)/elapsed.Seconds())
+
+			start = time.Now()
+		}
+	}
+
+}
